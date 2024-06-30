f9791e746771f04f568f5b7d6f2d05587c8c4a3c
mohammad <mohammadakhlaghi843@gmail.com>
2024-06-30 16:50:25 +0200

    header created

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+let humbergetToggle = document.querySelector('.humbergur_toggle--wrapper')
+let mobileMenu = document.querySelector('.mobile-menu')
+let mobileMenuList = document.querySelector('.mobile-menu-list')
+let humberger = document.querySelector('.hamburger-toggle')
+let humbergerAfter = document.querySelector('.hamburger-toggle-after')
+let humbergerBefore = document.querySelector('.hamburger-toggle-before')
+let header = document.querySelector('.header')
+let lastScroll = null
+let flag = false
+
+humbergetToggle.addEventListener('click',()=>{
+      if(!flag){
+        flag = true
+        humbergerAfter.classList.add('rotateReset')
+        humbergerBefore.classList.add('rotate')
+        humberger.style.display = 'none'
+        mobileMenuList.classList.remove('hide')
+        mobileMenu.classList.remove('hide')
+      }else{
+        flag = false
+        humbergerBefore.classList.remove('rotate')
+        humbergerAfter.classList.remove('rotateReset')
+        humberger.style.display = 'block'
+        mobileMenuList.classList.add('hide')
+        mobileMenu.classList.add('hide')
+      }
+})
+
+   window.addEventListener('scroll',()=>{
+         
+         if(window.scrollY > 400){
+              header.classList.add('active')
+              if(humberger.style.display === 'none'){
+                mobileMenuList.classList.remove('hide')
+                mobileMenu.classList.remove('hide')
+              }else{
+                mobileMenuList.classList.add('hide')
+                mobileMenu.classList.add('hide')
+              }
+              
+         }else{
+               header.classList.remove('active')
+         }
+         
+   })
