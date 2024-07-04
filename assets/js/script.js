b362c74342ac2ab76634d38e7f228eec41fb6709
Mohammad (FrontEnd Developer) <mohammadakhlaghi843@gmail.com>
2024-07-05 00:01:30 +0430

    trendig Games Developed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,10 @@ let humberger = document.querySelector('.hamburger-toggle')
 let humbergerAfter = document.querySelector('.hamburger-toggle-after')
 let humbergerBefore = document.querySelector('.hamburger-toggle-before')
 let header = document.querySelector('.header-menu-section')
+const trendingGamesContainer = document.querySelector('.trending-games__container')
 let flag = false
+
+//  humburger toggle feature
 
 humbergetToggle.addEventListener('click',()=>{
       if(!flag){
@@ -24,6 +27,8 @@ humbergetToggle.addEventListener('click',()=>{
         mobileMenu.classList.add('hide')
       }
 })
+
+//     sticy scroll feature
 
    window.addEventListener('scroll',()=>{
          
@@ -44,10 +49,38 @@ humbergetToggle.addEventListener('click',()=>{
    })
    
 
-   window.addEventListener('load',()=>{
-      fetch('http://localhost:3000/trending Games')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-      })
+  //  getting trending Games feature
+
+   window.addEventListener('load',async ()=>{
+      try {
+        const response = await fetch('http://localhost:3000/trending Games')
+        const trendingGames = await response.json()
+        let data = trendingGames.forEach((game) => {
+          console.log(game)
+             trendingGamesContainer.insertAdjacentHTML('beforeend',`
+                  <div class="trending-game__wrapper">
+                        <div class="trending-game-img__wrapper">
+                            <a href="#"><img src="${game.img}"></a>
+                            <span class="trending-game__discount">
+                               ${game.discount > 0 ? `<del>$${game.discount}</del> <P>$${game.price}</P>`
+                                :`<p class="no-discount">$${game.price}</p>`}
+                            </span>
+                        </div>
+                        <div class="trending-game-title__wrapper">
+                            <p class="trending-game__kind">Action</p>
+                            <div class="trending-game__title">
+                                <p>${game.title}</p>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                        <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clip-rule="evenodd" />
+                                      </svg>  
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+              `)
+        })
+      } catch (error) {
+        console.log('لطفا سرور را ران کنید')
+      }
    })
