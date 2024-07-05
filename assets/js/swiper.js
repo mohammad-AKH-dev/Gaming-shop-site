3675e2566e9f37ad7909c129185fd3df938834c7
Mohammad (FrontEnd Developer) <mohammadakhlaghi843@gmail.com>
2024-07-06 03:19:02 +0430

    Most played games section complited

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,5 +2,12 @@
 const swiper = new Swiper('.swiper',{
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 4.5,
+    navigation: {
+        prevEl:'.prev-btn',
+        nextEl:'.next-btn'
+    },
+    autoplay:true,
+    delay:3500,
+    speed:1000,
 })
